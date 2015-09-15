9e350f083befd367241ee73fd129dcbdff69237f
Sergey Ermolaev <whogood@gmail.com>
2015-09-15 16:06:20 +0700

    Initial commit with some js files

---
+++ README.md
@@ -0,0 +1,1 @@
+Repository for temporary files
---
+++ widget.js
@@ -0,0 +1,26 @@
+(function () {
+  function getReferrer() {
+    var referrer = document.referrer;
+    if (referrer) {
+      var linkEl = document.createElement('a');
+      linkEl.setAttribute('href', referrer);
+
+      return linkEl.hostname;
+    }
+
+    return '';
+
+  }
+
+  function getScript (filename) {
+    document.write('<' + 'script src="' + filename + '" type="text/javascript"><' + '/script>');
+  }
+
+  var ref = getReferrer();
+  var scriptName = 'id.js';
+  if (ref) {
+    scriptName += '?original_referer=' + ref;
+  }
+  getScript(scriptName);
+
+}());
